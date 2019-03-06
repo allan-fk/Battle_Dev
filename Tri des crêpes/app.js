52d7c1eddf51eea091cb4fec539e4495be1688b8
allan <allankleinpro@gmail.com>
2019-03-06 22:44:04 +0100

    test mirroring github

---
+++ Tri des crêpes/app.js
@@ -0,0 +1,14 @@
+const vardump = require("@smartankur4u/vardump");
+module.exports = {
+    ContestResponse: function () {
+        input = JSON.parse("[" + input + "]"); // Convert str to arr
+        maxVal = Math.max(...input); // Max val of arr
+        indexVal = input.indexOf(maxVal); // Index of max val
+        count = 0;
+        while (input != input.sort) {
+            input.reverse()
+            count++;
+        }
+        // console.log(input.sort());
+    }
+};
